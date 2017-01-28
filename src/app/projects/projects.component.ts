796ed44303296d8ea6a3a39a5d277fdd7a4264dd
yev <josephharveyangeles@gmail.com>
2017-01-28 10:03:41 -0800

    Implemented a vertical return button - changed positioning implementation of nav-buttons

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -20,6 +20,22 @@ import {ProjectItem} from './projectlist/project-item';
                     height: 0
                 }))
             ])
+        ]),
+        trigger('returnButton', [
+            state('in', style({ bottom: '22%', left: '-0.5%' })),
+            transition('void => *', [
+                style({
+                    bottom: 0,
+                    left: '-0.5%'
+                }),
+                animate('0.2s ease-in')
+            ]),
+            transition('* => void', [
+                animate('0.2s 10 ease-out', style({
+                    bottom: 0,
+                    left: '-0.5%'
+                }))
+            ])
         ])
     ],
     templateUrl: './projects.component.html',
