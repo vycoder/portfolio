9dbc6590fc7513dbfa086952eb705f749d3b8f39
yev <josephharveyangeles@gmail.com>
2017-02-02 08:50:14 -0800

    Updated manifesto

--- src/app/landingpage/sections/services/contents/manifesto.static.content.ts
+++ src/app/landingpage/sections/services/contents/manifesto.static.content.ts
@@ -1,5 +1,5 @@
 export const MANIFESTO = {
     figureSource: "public/images/profile.png",
     title: "manifesto",
-    content: "I think that creating computer programs is a discipline of both Science and Art mungled together in perfect harmony. Building client-oriented software application is more than that though, It's a mixture of techninal excellence and two-way communication between the client and the engineer, and I always strive to deliver both."
+    content: "I think that creating computer programs is a discipline of both Science and Art mungled together in perfect harmony. Building user-oriented software application is more than that though, It's a mixture of techninal excellence and two-way communication between the client and the engineer, and I always strive to deliver both."
 }
