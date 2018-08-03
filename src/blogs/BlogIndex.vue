bfde8b5bed13deebfaa59534cb9c34cd5ba9ed3b
yev <josephharveyangeles@gmail.com>
2018-08-03 23:36:06 +0800

    Implemented featured blog page

--- src/blogs/BlogIndex.vue
+++ src/blogs/BlogIndex.vue
@@ -2,54 +2,35 @@
   <div>
     <h6 class="series text-secondary font-condensed-roboto q-mb-lg">CodeNo0b's Guide Out of Mediocrity</h6>
     <div class="row gutter-x-md cngom">
-      <div class="div col-4 col-md-4 col-sm-6 col-xs-12" >
-        <q-card>
-          <q-card-media>
-            <img src="statics/images/blogs/codenoobsguide/main.png">
-          </q-card-media>
-          <q-card-main>
-            <p class="title text-secondary">main() - Prelude</p>
-            <p class="subtitle text-grey-8">July 8, 2017</p>
-          </q-card-main>
-        </q-card>
-      </div>
-      <div class="div col-4 col-md-4 col-sm-6 col-xs-12" >
-        <q-card>
-          <q-card-media>
-            <img src="statics/images/blogs/codenoobsguide/main.png">
-          </q-card-media>
-          <q-card-main>
-            <p class="title text-secondary">main() - Prelude</p>
-            <p class="subtitle text-grey-8">July 8, 2017</p>
-          </q-card-main>
-        </q-card>
-      </div>
-      <div class="div col-4 col-md-4 col-sm-6 col-xs-12" >
-        <q-card>
-          <q-card-media>
-            <img src="statics/images/blogs/codenoobsguide/main.png">
-          </q-card-media>
-          <q-card-main>
-            <p class="title text-secondary">main() - Prelude</p>
-            <p class="subtitle text-grey-8">July 8, 2017</p>
-          </q-card-main>
-        </q-card>
+      <div
+        v-for="(entry, k) in codeNoobsGuides"
+        :key="k"
+        class="div col-4 col-md-4 col-sm-6 col-xs-12 q-mb-md" >
+        <thumbnail
+          :image="`statics/images/blogs/${entry.image}`"
+          :to="`blog/${entry.to}`"
+          :title="entry.title"
+          :subtitle="entry.date" />
       </div>
     </div>
   </div>
 </template>
+<script>
+import DATA from 'statics/data/blogs.json'
+import Thumbnail from 'components/blogs/Thumbnail'
+export default {
+  name: 'BlogIndexPage',
+  components: {Thumbnail},
+  created () {
+    this.codeNoobsGuides = DATA.featured.codeNoobsGuide
+  }
+}
+</script>
+
 <style lang="stylus" scoped>
 @import '~variables'
 .series {
   border-bottom 1px solid $secondary
   padding-bottom .5rem
-}
-.title {
-  font-weight 500
-  font-size .9rem
-  margin 0
-}
-.subtitle {
-  margin 0
 }
 </style>
---
+++ src/components/blogs/Thumbnail.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-card>
+    <q-card-media @click.native="$router.push(to)">
+      <img :src="image">
+    </q-card-media>
+    <q-card-main>
+      <p class="title text-secondary">{{title}}</p>
+      <p class="subtitle text-grey-8">{{subtitle}}</p>
+    </q-card-main>
+  </q-card>
+</template>
+<script>
+export default {
+  name: 'ThumbnailComponent',
+  props: ['image', 'title', 'subtitle', 'to']
+}
+</script>
+
+<style lang="stylus" scoped>
+.q-card {
+  cursor pointer
+  transition all .5s ease
+  &:hover {
+    box-shadow 0 .5rem 2rem rgba(black, .5)
+    transform translateY(-3px)
+  }
+}
+.title {
+  font-weight 500
+  font-size .9rem
+  margin 0
+}
+.subtitle {
+  margin 0
+}
+</style>
