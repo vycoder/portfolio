973673eb6b3f940d0da68a4bc55ca1e33b0865fa
yev <josephharveyangeles@gmail.com>
2022-07-31 21:07:57 +0800

    Added new Vertex page

---
+++ src/pages/Vertex.vue
@@ -0,0 +1,22 @@
+<template>
+  <Layout>
+    <div class="container-inner mx-auto py-16">
+      <h2 class="text-4xl font-bold mb-16 text-center">Vertex</h2>
+    </div>
+    <div class="container-inner mx-auto py-20">
+      <g-image src="../../static/dancingkitty.gif" alt="Dancing Kitty" class="mx-auto" />
+    </div>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Vertex'
+  },
+  created() {
+    window.location.href = process.env.GRIDSOME_VERTEX_URL
+  }
+}
+</script>
